60bb4bd8d4dd3a96782c25634028ef71ecd517e5
qinjingyi.jay <qinjingyi.jay@bytedance.com>
2022-02-14 20:36:05 +0800

    一个textView的更新，检查是否getAdapter成功

--- app/src/main/java/com/example/bluetoothlearning/MainActivity.java
+++ app/src/main/java/com/example/bluetoothlearning/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.bluetoothlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn_1);
+        tv1 = findViewById(R.id.tv_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (bluetoothAdapter == null) {
+                    // Device doesn't support Bluetooth
+                    tv1.setText("并不支持蓝牙");
+                    return;
+                }
+                else {
+                    tv1.setText("支持蓝牙");
+                }
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
